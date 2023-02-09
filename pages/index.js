3ade18868f888f7a8d818271c88671c60c3875d9
naga <yuto7687@gmail.com>
2023-02-10 02:57:20 +0900

    ✨ Headerコンポーネントの作成

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Header from '@/components/Header'
+import Nav from '@/components/Nav'
 import Head from 'next/head'
 
 export default function Home() {
@@ -7,7 +9,13 @@ export default function Home() {
         <title>Hulu Clone App</title>
         <link rel="icon" href="/Hulu_ico.png" />
       </Head>
+
+      <Header/>
+
+      {/* Nav */}
       <h1>Let&apos;s build Hulu</h1>
+
+      {/* Result */}
     </>
   )
 }
